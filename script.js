28cd96ff7a5549f7b6ab0688fec1dce5de7bf44b
sara <sarahuangg7@gmail.com>
2020-02-28 02:48:44 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,66 @@
+function delayTime() {
+  setTimeout(function(){
+      var showDiv = document.getElementById("gameover"); 
+      showDiv.style.display = "block";
+   }, 9000);
+}
+
+
+delayTime();
+
+document.getElementById("circle1").onclick=function() {
+    document.getElementById("circle1").style.display="none";
+}
+
+
+function myFunction() {
+    document.getElementById("message").style.display = "block";
+  }
+
+
+document.getElementById("circle3").onclick=function() {
+    document.getElementById("circle3").style.display="none";
+}
+
+
+
+
+document.getElementById("circle4").onclick=function() {
+    document.getElementById("circle4").style.display="none";
+}
+
+
+document.getElementById("circle5").onclick=function() {
+    document.getElementById("circle5").style.display="none";
+}
+
+
+
+document.getElementById("circle6").onclick=function() {
+    document.getElementById("circle6").style.display="none";
+}
+
+document.getElementById("circle7").onclick=function() {
+    document.getElementById("circle7").style.display="none";
+}
+
+document.getElementById("circle8").onclick=function() {
+    document.getElementById("circle8").style.display="none";
+}
+
+
+document.getElementById("circle9").onclick=function() {
+    document.getElementById("circle9").style.display="none";
+}
+
+
+
+document.getElementById("circle10").onclick=function() {
+    document.getElementById("circle10").style.display="none";
+}
+
+
+
+document.getElementById("circle11").onclick=function() {
+    document.getElementById("circle11").style.display="none";
+}
